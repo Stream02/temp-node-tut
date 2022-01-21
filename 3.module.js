51c6b4c497dcdf686ba376f874c03325e6e8729a
Stream02 <79915713+Stream02@users.noreply.github.com>
2022-01-21 10:35:40 +0530

    first commit

---
+++ 3.module.js
@@ -0,0 +1,7 @@
+const {john,peter} = require('./4.names');
+const sayhi = require('./5.utils')
+const data = require('./6.alternative')
+require('./7.mind grenade')
+ sayhi('susan')
+ sayhi(john)
+ sayhi(peter)
